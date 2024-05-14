322de84b23a0ba4697fae897a88b2f802ccebfc4
AxGhost1 <155352166+AxGhost1@users.noreply.github.com>
2024-05-14 18:41:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(800, 800);
+  
+    background(200);
+    
+    circle(200, 200, 50);
+    
+    circle(600, 200, 50);
+    
+    circle(600, 600, 50);
+      
+    circle(200, 600, 50);
+  
+    describe("A white circle with black outline in the middle of a gray canvas.");
+  }
+  
